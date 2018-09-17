5e0dce65d690ff86635616688e9249420c1604e2
Cody Reed <cody2j2reed@gmail.com>
2018-09-17 14:47:01 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div>
+      <h3>Cody Reed</h3>
+      <Nav />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
